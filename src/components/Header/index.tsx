e9a505fa28d926afae5b8a57c6fec9ab550b0b08
Oliver Mayer <oliver.mayer@sponte.com.br>
2023-05-31 20:12:07 -0300

    ajuste espaçamento autores

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,14 +5,7 @@ export function Header() {
     <HeaderContainer>
       <h1>Códigos Auxiliares</h1>
       <p>
-        By
-        <a href="https://github.com/oliver-zyn" target="_blank">
-          Oliver Mayer
-        </a>
-        &
-        <a href="https://github.com/Vttrium" target="_blank">
-          Pedro Sauthier
-        </a>
+        By <a href="https://github.com/oliver-zyn" target="_blank"> Oliver Mayer</a> & <a href="https://github.com/Vttrium" target="_blank"> Pedro Sauthier</a>
       </p>
     </HeaderContainer>
   )
