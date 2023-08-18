155d6c971a28322d49c1cf4cc838ae5b0c8d6b5b
oliver-zyn <olivermayer15@gmail.com>
2023-08-17 21:28:58 -0300

    removendo códigos desnecessários

--- src/pages/SelectMenu/index.tsx
+++ src/pages/SelectMenu/index.tsx
@@ -1,65 +1,6 @@
-// import { useState } from "react";
-// import { SelectHeader } from "./components/SelectHeader";
-// import { SelectList } from "./components/SelectList";
-
 import { SelectCard } from "./components/SelectCard";
 
-// const periodContents = [
-//   {
-//     period: 1,
-//     techs: [
-//       {
-//         icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg',
-//         name: 'Linguagem C',
-//         link: ''
-//       }
-//     ]
-//   },
-//   {
-//     period: 2,
-//     techs: [
-//       {
-//         icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg',
-//         name: 'Linguagem C',
-//         link: ''
-//       },
-//       {
-//         icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
-//         name: 'Java',
-//         link: ''
-//       },
-//       {
-//         icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
-//         name: 'SQL',
-//         link: ''
-//       }
-//     ]
-//   },
-//   {
-//     period: 3,
-//     techs: []
-//   },
-//   {
-//     period: 4,
-//     techs: []
-//   },
-//   {
-//     period: 5,
-//     techs: []
-//   },
-//   {
-//     period: 6,
-//     techs: []
-//   },
-// ]
-
 export function SelectMenu() {
-  // const [periodActive, setPeriodActive] = useState(1);
-
-  // function handleSelectPeriodActive(period: number) {
-  //   setPeriodActive(period)
-  // }
-
   return (
     <>
       <SelectCard />
